de42feadf27cd0f6a8ea386766a2ce1700c01005
Filipe de Andrade Morais <filipe.morais@ul.com>
2017-11-17 17:31:39 -0200

    Inicio do projeto

---
+++ src/alura_selenium/TesteAutomatizado.java
@@ -0,0 +1,39 @@
+package alura_selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.GeckoDriverService;
+
+public class TesteAutomatizado {
+	
+	
+	public static void main (String[] args) {
+	
+	FirefoxDriver driver;
+		
+	// por ser uma nova versão preciso setar o webdriver
+	System.setProperty("webdriver.firefox.marionette", "C:\\Users\\12129\\OneDrive - Underwriters Laboratories\\11.Test Automation\\Selinium\\geckodriver-v0.19.1-win64\\geckodriver.exe");
+	driver = new FirefoxDriver();
+	
+	// acessa o site do google
+	driver.get("http://www.google.com.br/");
+	
+	// digita no campo com nome "q" do google
+	WebElement campoDeTexto = driver.findElement(By.name("q"));
+	campoDeTexto.sendKeys("Caelum");
+	
+	// submete o form
+	campoDeTexto.submit();
+	
+/*	public static void main(String[] args) {
+		
+		TesteAutomatizado myObj = new TesteAutomatizado();
+		myObj.invokeBrowser();
+
+	}*/
+
+	
+	}
+}
